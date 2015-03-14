f6dce8508e708ad9dda096894dc3ddf9ca8267d2
Marcel Vidale <mvidale@gmail.com>
2015-03-14 12:17:10 +0000

    Changes made by Marcel Vidale (mvidale@gmail.com).

--- CABFinder/app/_2001_taxi_reg_01.js
+++ CABFinder/app/_2001_taxi_reg_01.js
@@ -3,6 +3,7 @@ var dialogs = require("ui/dialogs");
 var vmModule = require("./view-models/view_model");
 var frameModule = require("ui/frame");
 var LocationManager = require("location").LocationManager;
+var Location = require("location").Location;
 var locationManager = new LocationManager();
 
 function pageLoaded(args) {
@@ -14,14 +15,18 @@ exports.pageLoaded = pageLoaded;
 function buttonTap3(args) {
     var sender = args.object;
     var parent = sender.parent;
-    
+
     var isEnabled = LocationManager.isEnabled();
     dialogs.alert("Location Services: " + isEnabled);
 
     var lbl = view.getViewById(parent, "Label1");
+    var locNewYork = new Location();
+    locNewYork.longitude = 40.71448;
+    locNewYork.latitude = -74.00598;
 
     locationManager.startLocationMonitoring(function (location) {
-        lbl.text = 'Location received: ' + location.latitude;
+        var distance = LocationManager.distance(location, locNewYork);
+        lbl.text = 'NY Distance ' + distance + ' meters';
     }, function (error) {
         console.log('Location error received: ' + error);
     });
