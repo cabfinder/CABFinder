9c55aa30148db2d264387e1bb497a7e890965178
Marcel Vidale <mvidale@gmail.com>
2015-03-14 09:34:09 +0000

    Changes made by Marcel Vidale (mvidale@gmail.com).

---
+++ CABFinder/app/view-models/view-model.js
@@ -0,0 +1,18 @@
+
+var observable = require("data/observable");
+
+
+var counter = 42;
+var mainViewModel = new observable.Observable();
+mainViewModel.set("message", counter + " taps left");
+mainViewModel.tapAction = function () {
+    counter--;
+    if (counter <= 0) {
+        mainViewModel.set("message", "Hurrah! You unlocked the NativeScript clicker achievement!");
+    } else {
+        mainViewModel.set("message", counter + " taps left");
+    }
+};
+exports.mainViewModel = mainViewModel;
+
+
