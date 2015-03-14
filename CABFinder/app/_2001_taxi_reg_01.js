dd9fda3ccea9e284eed540d8f3231c7496cf8aaf
Marcel Vidale <mvidale@gmail.com>
2015-03-14 09:35:18 +0000

    Changes made by Marcel Vidale (mvidale@gmail.com).

--- CABFinder/app/_2001_taxi_reg_01.js
+++ CABFinder/app/_2001_taxi_reg_01.js
@@ -1,14 +1,27 @@
 var view = require("ui/core/view");
+var dialogs = require("ui/dialogs");
+var vmModule = require("./view-models/view-model");
 
-function buttonTap(args) {
 
-    var sender = args.object;
-    var parent = sender.parent;
-    if (parent) {
-        var lbl = view.getViewById(parent, "Label1");
-        if (lbl) {
-            lbl.text = "Success";
-        }
-    }
+function pageLoaded(args) {
+    var page = args.object;
+    page.bindingContext = vmModule.mainViewModel;
 }
-exports.buttonTap = buttonTap;
+exports.pageLoaded = pageLoaded;
+
+
+function buttonTap2(args) {
+    dialogs.confirm("Do you wish to continue?").then(function (result) {
+        if (result) {
+            var sender = args.object;
+            var parent = sender.parent;
+            if (parent) {
+                var lbl = view.getViewById(parent, "Label1");
+                if (lbl) {
+                    lbl.text = "Confirmed";
+                }
+            }
+        }
+    });
+}
+exports.buttonTap2 = buttonTap2;
